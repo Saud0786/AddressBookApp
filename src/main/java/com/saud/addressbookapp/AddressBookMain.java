90f41e8446bbedd3eaa4a90f049cf548e4363247
Saud0786 <saud31253@gmail.com>
2026-03-09 15:53:09 +0530

    [Saud]: added count state and city in address app

--- src/main/java/com/saud/addressbookapp/AddressBookMain.java
+++ src/main/java/com/saud/addressbookapp/AddressBookMain.java
@@ -1,3 +1,4 @@
+
 package com.saud.addressbookapp;
 
 import java.util.Scanner;
@@ -80,7 +81,9 @@ public class AddressBookMain {
             System.out.println("6 Search by State");
             System.out.println("7 View Persons by City");
             System.out.println("8 View Persons by State");
-            System.out.println("9 Back");
+            System.out.println("9 Count by City ");
+            System.out.println("10 Count by State");
+            System.out.println("11 Back");
 
             System.out.print("Enter choice: ");
             int choice = sc.nextInt();
@@ -173,6 +176,24 @@ public class AddressBookMain {
                     break;
 
                 case 9:
+
+                    System.out.println("Enter city:");
+                    String cityCount = sc.nextLine();
+
+                    book.countByCity(cityCount);
+
+                    break;
+                    
+                case 10:
+
+                    System.out.println("Enter state:");
+                    String stateCount = sc.nextLine();
+
+                    book.countByState(stateCount);
+
+                    break;
+                    
+                case 11:
                     return;
                     
                 default:
