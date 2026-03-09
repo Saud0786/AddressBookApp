a0ae27993263738ed0be511feb2ef446bc300920
Saud0786 <saud31253@gmail.com>
2026-03-09 12:47:19 +0530

    [Saud]: added Edit feature in address app

--- src/main/java/com/saud/addressbookapp/AddressBookMain.java
+++ src/main/java/com/saud/addressbookapp/AddressBookMain.java
@@ -1,22 +1,54 @@
 package com.saud.addressbookapp;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Welcome to Address Book Program");
 
-        Contact contact = new Contact(
-                "Saud",
-                "Saud",
-                "Bhopal",
-                "Bhopal",
-                "MP",
-                "462022",
-                "9876543210",
-                "saud@gmail.com"
-        );
+        System.out.println("Enter First Name:");
+        String firstName = sc.nextLine();
 
-        contact.displayContact();
+        System.out.println("Enter Last Name:");
+        String lastName = sc.nextLine();
+
+        System.out.println("Enter Address:");
+        String address = sc.nextLine();
+
+        System.out.println("Enter City:");
+        String city = sc.nextLine();
+
+        System.out.println("Enter State:");
+        String state = sc.nextLine();
+
+        System.out.println("Enter Zip:");
+        String zip = sc.nextLine();
+
+        System.out.println("Enter Phone:");
+        String phone = sc.nextLine();
+
+        System.out.println("Enter Email:");
+        String email = sc.nextLine();
+
+        Contact contact = new Contact(firstName,lastName,address,city,state,zip,phone,email);
+
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.addContact(contact);
+
+        System.out.println("\nContact Details:");
+        addressBook.displayContact();
+
+        System.out.println("\nEnter the first name to edit contact:");
+        String name = sc.nextLine();
+
+        addressBook.editContact(name);
+
+        System.out.println("\nUpdated Contact:");
+        addressBook.displayContact();
     }
 }
